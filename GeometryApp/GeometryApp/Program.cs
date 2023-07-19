c1204dadf851338af29fed7ee86e86fd37b12ffe
Dark-MonkGI <gryaznov774@gmail.com>
2023-07-19 23:06:43 +0400

    Added a console application to test the library's functionality

---
+++ GeometryApp/GeometryApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using AreaCalculationLib;
+
+namespace GeometryApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Проверяем работоспособность библиотеки:
+                        
+            Circle circle = new Circle(5);
+            Triangle triangle = new Triangle(3, 4, 5);
+
+
+            Console.WriteLine(CalculatedArea.CalculateArea(circle));
+            Console.WriteLine(CalculatedArea.CalculateArea(triangle));
+            Console.WriteLine(CalculatedArea.IsRightAngledTriangle(triangle));
+        }
+    }
+}
